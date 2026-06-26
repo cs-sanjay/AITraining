5c15cceb30cfdf015dfb89e3f0577f7c0cfc4591
Sanjay Kumar <66733043+cs-sanjay@users.noreply.github.com>
2026-06-26 14:49:45 +0530

    Update stlit.py

--- stlit.py
+++ stlit.py
@@ -1,37 +1,50 @@
 import streamlit as st
-from google import genai as M
-robo = M.Client(api_key=st.secrets["MY_API"])
-mychat = robo.chats.create(model="gemini-flash-lite-latest")
-##st.title("My AI")
-##question = st.text_input("Ask Question")
-##if st.button("Send"):
-##    response = mychat.send_message(question)
-##    st.write(response.text)
-
+from google import genai
+from google.genai import types
+config = types.GenerateContentConfig(
+    system_instruction = ".You are an expert Python developer.
+ Answer only questions related to Python programming.
+ For any non-Python question, reply exactly:
+ Please ask a Python-related question.
+ Do not answer questions outside the Python domain."
+  )
 st.markdown(
   """
   <h1 style='text-align: center;'> Python AI Assistant</h1>
   <p style='text-align: center; font-size:18px;'>
-    Ask any GK question.
+    Ask any Python programming question.
   </p>
   """,
   unsafe_allow_html=True,
 )
+client = genai.Client( api_key=st.secrets["API_KEY"])
+mychat = robo.chats.create(model="gemini-flash-lite-latest")
 
+Placeholder for the response
 response_placeholder = st.empty()
-question = st.text_input("", placeholder="Entry your Question")
-col1, col2, col3 = st.columns((4, 1, 4))
+
+question = st.text_input("", placeholder="Enter your Python question here...")
+
+col1, col2, col3 = st.columns([4, 1, 4])
 
 with col2:
-    send = st.button("Send")
+  send =st.button("Send")
+if send:
+  response = mychat.send_message(question)
+  response_placeholder.write(response.text)
 
+mychat = robo.chats.create(model="gemini-flash-lite-latest")
+#Placeholder for the response
+response_placeholder = st.empty()
+
+question = st.text_input("", placeholder="Enter your Python question here...")
+
+col1, col2, col3 = st.columns([4, 1, 4])
+
+with col2:
+  send =st.button("Send")
 
 if send:
-    response = mychat.send_message(question)
-    response_placeholder.write(response.text)
-
-
-  
-
-
- 
+  question = question + config.system_instruction
+  response = mychat.send_message(question)
+  response_placeholder.write(response.text)
